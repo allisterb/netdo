1df5f5e15a4ba6ec74b4232b75869ef6b125472f
Allister Beharry <allister.beharry@gmail.com>
2026-03-13 23:43:46 -0400

    Add SpacesClient class.

---
+++ src/NetDo.Api/SpacesClient.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalOcean.Api;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Amazon.S3;
+public class SpacesClient : Runtime
+{
+    public SpacesClient(string bucketName)
+    {
+        this.bucketName = bucketName;
+    }
+
+    #region Fields
+    protected readonly string bucketName;
+    #endregion
+}
