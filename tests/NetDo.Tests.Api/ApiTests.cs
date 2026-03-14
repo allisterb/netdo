094a65d13698bdc619a336bcec46f2356405fab5
Allister Beharry <allister.beharry@gmail.com>
2026-03-14 13:01:21 -0400

    Add spaces* test vars

--- tests/NetDo.Tests.Api/ApiTests.cs
+++ tests/NetDo.Tests.Api/ApiTests.cs
@@ -7,9 +7,13 @@ public class ApiTests : TestsRuntime
     static ApiTests()
     {
         apiKey = GetRequiredValue(config, "ApiKey");
+        spacesEndpoint = GetRequiredValue(config, "SpacesEndpoint");
+        spacesKeyId = GetRequiredValue(config, "SpacesKeyId");
+        spacesKeySecret = GetRequiredValue(config, "SpacesKeySecret");
     }
     
     protected static string apiKey;
+    protected static string spacesEndpoint, spacesKeyId, spacesKeySecret;
 
     protected DigitalOceanClient client = new DigitalOceanClient(apiKey);
 }
